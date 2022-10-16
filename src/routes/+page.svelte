dd0f4a0ae839e3ba3870903289f15f96e5050a8f
Julian Veerkamp <julian@veerkamp.me>
2022-10-16 13:35:50 +0200

    Stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let count = 0;
+
+	function handleClick () {
+		count = count + 1;
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Counter: {count}</p>
+
+<button on:click={handleClick}>+1</button>
